6613134506d64176479ee9f363ce5861b78a6223
fwldom <fwldom@duck.com>
2024-09-16 18:12:35 +0330

    Update 1.3.1 (Add Background)

--- renderer.js
+++ renderer.js
@@ -231,7 +231,7 @@ function Onload() {
         }
     };
     // Load Background
-    backgroundList = ["1.jpg", "2.png","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg"];
+    backgroundList = ["1.jpg", "2.png","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg"];
     function getRandomImage() {
         const randomIndex = Math.floor(Math.random() * backgroundList.length);
         return "assets/background/" + backgroundList[randomIndex];
